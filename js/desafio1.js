e049e5c22a31986586db85ac235b24c08a4717e3
Joel Meza <124853523+Zicrown@users.noreply.github.com>
2024-04-03 12:58:14 -0400

    First Commit

---
+++ js/desafio1.js
@@ -0,0 +1,8 @@
+"use strict"
+let nombre = "Joel";
+let cedula = 24340176;
+let casado = true;
+
+console.warn(`Nombre: ${nombre}; Tipo de Dato: ${typeof(nombre)}`);
+console.warn(`Cedula: ${cedula}; Tipo de Dato: ${typeof(cedula)}`);
+console.warn(`Casado: ${casado}; Tipo de Dato: ${typeof (casado)}`);
